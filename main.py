be36fec4d380515c0a698fbb107c781b203eed9b
alvin <aocasiojr@my.nl.edu>
2023-10-03 15:09:35 -0500

    printing tabs

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-print("today is a good day to learn python")
-print("python is fun")
-print("python's string are easy to use")
-print("hello" + "world")
-greeting = "hello"
-name = input("pleas enter your name ")
-print(greeting + name)
-# if we want a space, we can add that too print(greeting + ' ' + name)
+print ("number 1\tAmerican Elm")
+print ("number 2\tSugar Maple")
+print ("number 3\tred Oak")
+print ("number 4\tAmerican Beech")
